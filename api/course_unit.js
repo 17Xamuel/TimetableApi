1ddfd7ee6118aec40080a7e789fecf276a246195
XamuelUG <samweko@gmail.com>
2022-02-22 12:51:25 +0300

    course unit

--- api/course_unit.js
+++ api/course_unit.js
@@ -3,19 +3,24 @@ const { CourseUnit } = require("../models/Models");
 
 //new
 router.post("/new", async (req, res) => {
-  console.log(req.body);
   const course_unit_check = await CourseUnit.findOne({
     course_unit_name: { $eq: req.body.course_unit_name },
   });
   if (course_unit_check) {
     res.send({ data: "Course Unit Exists", status: false });
   } else {
+    const codes = [];
+    Object.keys(req.body).forEach((key) => {
+      if (key.substring(12, 15) === "cod") {
+        codes.push(req.body[key]);
+      }
+    });
     const course_unit = new CourseUnit({
       course_unit_name: req.body.course_unit_name,
       credit_units: parseInt(req.body.course_unit_credit_units),
       course_unit_teacher: req.body.course_unit_teacher,
-      course_unit_room: req.body.select_room,
-      course_unit_codes: JSON.stringify(req.body.course_unit_codes),
+      course_unit_room: req.body.course_unit_room,
+      course_unit_codes: JSON.stringify(codes),
     });
     try {
       const saved_course_unit = await course_unit.save();
