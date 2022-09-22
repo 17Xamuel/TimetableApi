718fd6760db5f0e1db0d2ce3e080a8bd68e939ee
XamuelUG <samweko@gmail.com>
2022-09-22 22:18:13 +0300

    added public

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 //middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 app.use("/api/users/", require("./api/user"));
 app.use("/api/rooms/", require("./api/room"));
 app.use("/api/class/", require("./api/class"));
@@ -15,9 +16,9 @@ app.use("/api/course-units/", require("./api/course_unit"));
 
 conn();
 
-app.get("/", (req, res) => {
-  res.send("Home");
-});
+// app.get("/", (req, res) => {
+//   res.send("Home");
+// });
 
 app.listen(PORT, () => {
   console.log(`Server Listening on PORT ${PORT}`);
